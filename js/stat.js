76c03d598827a00d926c85cdf3a27e3bc8b5e609
Alena <chestnaja_alena@mail.ru>
2019-06-07 14:09:59 +0300

    Вывела значения времени и нарисовала колонки соответствующей высоты и цвета

--- js/stat.js
+++ js/stat.js
@@ -2,11 +2,20 @@ window.renderStatistics = function(ctx, names, times) {
   var startLeft = 130;
   var widthOfColumn = 40;
   var marginRightOfColumn = 50;
+  var maxHeightOfColumn = 150;
+  var marginTopOfHistogram = 95;
+
   var calculateStartX = function(numberOfColumn) {
-    return startLeft + (widthOfColumn + marginRightOfColumn) * numberOfColumn ;
+    return startLeft + (widthOfColumn + marginRightOfColumn) * numberOfColumn;
+  }
+  var calculateStartY = function(heightOfColumn) {
+    return marginTopOfHistogram + maxHeightOfColumn - heightOfColumn;
   }
 
+  var maxTime = Math.round(Math.max.apply(null, times));
+  var ratioHeightTime = maxHeightOfColumn / maxTime;
 
+  // рисуем облако с заголовком
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -16,7 +25,6 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.strokeStyle = 'grey';
   ctx.strokeRect(100, 10, 420, 270);
 
-
   ctx.fillStyle = 'black';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
@@ -24,32 +32,29 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.fillText('Список результатов:', 130, 50);
 
 
-/*  ctx.fillStyle = 'blue';
-  ctx.fillRect(calculateStartX(0), 80, widthOfColumn, 150);
-
-  ctx.fillStyle = 'pink';
-  ctx.fillRect(calculateStartX(1), 80, widthOfColumn, 150);
-
-  ctx.fillStyle = 'lightgreen';
-  ctx.fillRect(calculateStartX(2), 80, widthOfColumn, 150);
-
-  ctx.fillStyle = 'yellow';
-  ctx.fillRect(calculateStartX(3), 80, widthOfColumn, 150);
-*/
-
   for (var i = 0; i < names.length; i++) {
-    var time = times[i];
+    var time = Math.round(times[i]);
     var name = names[i];
+    var heightOfColumn = time * ratioHeightTime;
 
+    // рисуем текст с именами и временем
     ctx.fillStyle = 'black';
     ctx.font = '16px PT Mono';
     ctx.textBaseline = 'hanging';
-    ctx.fillText(name, calculateStartX(i), 250);
+    ctx.fillText(name, calculateStartX(i), 255);
+    ctx.fillText(time, calculateStartX(i), calculateStartY(heightOfColumn) - 20);
 
-    console.log(time);
+    // задаем цвет колонкам
+    if (name == 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var RandomSaturation = Math.round(Math.random() * 100);
+      var saturateColor = 'hsl(255, ' + RandomSaturation + '%, 50%)';
+      ctx.fillStyle = saturateColor;
+    }
 
-    ctx.fillStyle = 'indianred';
-    ctx.fillRect(calculateStartX(i), 80, widthOfColumn, 150);
+    // рисуем колонки
+    ctx.fillRect(calculateStartX(i), calculateStartY(heightOfColumn), widthOfColumn, heightOfColumn);
+
   };
-
 };
